5a17466d2a122edb9bc536d70d9107d0ce001777
Andre Staltz <andre@staltz.com>
2017-07-11 19:58:10 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Propagate Replacement Fields
+
+> Take a package.json field and copy paste its contents to all package.json of modules inside `/node_modules`
+
+So let's imagine you have these replacement modules (e.g. for browserify) in your project's package.json:
+
+```
+  "browser": {
+    "stream": "stream-browserify",
+    "os": "os-browserify"
+  }
+```
+
+Or in your React Native project:
+
+```
+  "react-native": {
+    "crypto": "react-native-crypto",
+    "tcp": "react-native-tcp",
+    "fs": false
+  }
+```
+
+And you want those to be present also in the package.json for each module installed in your project's `/node_modules`. This is sometimes needed, specially for eccentric React Native projects using Node.js specific stuff.
+
+So that's when you use `propagate-replacement-fields`, like this:
+
+`npm install --save-dev propagate-replacement-fields`
+
+`./node_modules/.bin/propagate-replacement-fields --field=browser`
+
+And then that field `"browser"` from your project's package.json will be cloned also into the dependencies installed in node_modules.
+
+Note, if you want to specify where is the source `package.json`, you can pass its path to `--source`:
+
+`./node_modules/.bin/propagate-replacement-fields --field=browser --source=../over-there`
+
+# install
+
+`npm install propagate-replacement-fields`
+
+# usage
+
+`propagate-replacement-fields --help`
+
+```
+Usage: propagate-replacement-fields [options]
+
+Options:
+  --field, -f   package.json field with replacement modules           [required]
+  --source, -s  where to look for the source package.json.
+                default: process.cwd()
+  --help        Show help                                              [boolean]
+
+Examples:
+  propagate-replacement-fields --field=browser
+```
---
+++ index.js
@@ -0,0 +1,72 @@
+var fs = require("fs");
+var path = require("path");
+
+var argv = require("yargs")
+  .usage("Usage: $0 [options]")
+  .example("$0 --field=browser")
+  .option("field", {
+    alias: "f",
+    description: "package.json field with replacement modules"
+  })
+  .option("source", {
+    alias: "s",
+    description:
+      "where to look for the source package.json.\ndefault: process.cwd()"
+  })
+  .demandOption("field")
+  .help().argv;
+
+var sourcePath = argv.source ? argv.source : process.cwd();
+var sourcePackageJsonPath = path.join(sourcePath, "package.json");
+
+var sourcePackageJson = JSON.parse(
+  fs.readFileSync(sourcePackageJsonPath, "utf-8")
+);
+
+function insertShims(currentPath, field) {
+  if (!fs.existsSync(path.join(currentPath, "./node_modules"))) {
+    return;
+  }
+
+  var nodeModules = fs
+    .readdirSync(path.join(currentPath, "./node_modules"))
+    .map(m => path.join(currentPath, "./node_modules", m))
+    .filter(m => fs.statSync(m).isDirectory)
+    .filter(m => fs.existsSync(path.join(m, "package.json")));
+
+  nodeModules.forEach(modulePath => {
+    try {
+      var packageJsonPath = path.join(modulePath, "package.json");
+      var prevFileContent = fs.readFileSync(packageJsonPath, "utf-8");
+      var packageJson = JSON.parse(prevFileContent);
+      if (field in packageJson) {
+        Object.keys(sourcePackageJson[field]).forEach(moduleName => {
+          if (!(moduleName in packageJson[field])) {
+            packageJson[field][moduleName] =
+              sourcePackageJson[field][moduleName];
+          }
+        });
+      } else {
+        packageJson[field] = sourcePackageJson[field];
+      }
+      var nextFileContent = JSON.stringify(packageJson, null, "  ");
+      fs.writeFileSync(packageJsonPath, nextFileContent, "utf-8");
+
+      insertShims(modulePath, field);
+    } catch (e) {
+      console.error(e);
+    }
+  });
+}
+
+if (!(argv.field in sourcePackageJson)) {
+  console.error(
+    'ERROR: Field "' +
+      argv.field +
+      '" was not found in ' +
+      sourcePackageJsonPath +
+      ", so we can't propagate that."
+  );
+} else {
+  insertShims(sourcePath, argv.field);
+}
